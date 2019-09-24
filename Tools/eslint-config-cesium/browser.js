db3063f11e18276ee41102ea467cbd6ea2875415
Matthew Amato <matt.amato@gmail.com>
2019-09-24 08:48:24 -0400

    Update ESLint rules for Node.js

--- Tools/eslint-config-cesium/browser.js
+++ Tools/eslint-config-cesium/browser.js
@@ -7,7 +7,6 @@ module.exports = {
         browser: true
     },
     rules: {
-        'no-implicit-globals': 'error',
-        "no-prototype-builtins": "off"
+        'no-implicit-globals': 'error'
     }
 };
